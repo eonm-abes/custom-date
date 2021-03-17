98eedc8ca9028ff92c326af343077169ca1c4b33
Mathis EON <eon@abes.fr>
2021-03-17 10:34:16 +0100

    fix

--- src/lib.ts
+++ src/lib.ts
@@ -1,25 +1,24 @@
 interface DateConfig {
-  	month?: number;
-  	year?: number;
+  month?: number;
+  year?: number;
 }
 
 export class CustomDate {
-  	date: Date;
+  date: Date;
 
-  	constructor({ month = 0, year = 0 } : DateConfig = {}) {
-	    let date = new Date(Date.now());
+  constructor({ month = 0, year = 0 }: DateConfig = {}) {
+    let date = new Date(Date.now());
 
-	    date.setMonth(date.getMonth() + month);
-    	date.setFullYear(date.getFullYear() + year);
+    date.setMonth(date.getMonth() + month);
+    date.setFullYear(date.getFullYear() + year);
 
-	    this.date = date;
-  	}
+    this.date = date;
+  }
 
-	to_string(): string {
-    	return this.date.toISOString().split("T")[0];
-	}
+  to_string(): string {
+    return this.date.toISOString().split("T")[0];
+  }
 }
-
 
 // playground: https://www.typescriptlang.org/play?#code/JYOwLgpgTgZghgYwgAgCJ0gYQPYhsAc2QG8AoZZASAFtcwALAfgC5kQBXagI2gG5yqATwhwoLNpx5R+AX1KkEAGzgBnFckzsVYbNXSQSAygBMMEVvoj8jCXNqjsEOqAApiyWuHrIAvMgAMADTIwqK+AcgyyBZmOHiE4cQyAJSGlBQUihBgyKYGfiAQAO5oZi6WAHQg2EUuycnW6Rl5EBUq2QCydPQuLRUEnd11yADUHt0NAhQmZm3ZAGLsiooAmiKufQNgi8trosNjoVCTpE0UDMAqFS3hLfzTcqc6APr2oAR1rG8gRGQZVFBsuwoCBkBcrn0dABJADKAHkYWAoO86m0AA6KYBgFwAIgAKjjkgBtfwAXX4lDkjwUdmwWQqimwH0KJU02l0llRL2+H3q-FsIBUdNajOZxQ0Wh0ejKxE8DFYAFoAMwpCrcpEovk0wXChlMlwsiXs6WQNxHRUAVmCcvorAATKr1cifnVJkA
 // console.log(new CustomDate().to_string());
