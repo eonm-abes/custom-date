c45b5154f6046186796feef7abc95b4d7999fb96
Mathis EON <eon@abes.fr>
2021-03-17 10:32:29 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: { bundle: "./src/lib.ts"},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    library: "CustomDate",
+  },
+}; 
